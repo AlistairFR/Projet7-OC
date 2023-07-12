9060906eac1958b00091256f18ae2719ae074e5d
AlistairFR <jbwinckel62370@gmail.com>
2023-07-12 12:54:48 +0200

    AJOUT middleware auth

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,21 +1,23 @@
 const express = require("express");
+const auth = require("auth");
 const router = express.Router();
+
 
 const bookController = require("../controllers/book");
 
 // POST d'un nouveau livre
-router.post('/', bookController.createBook);
+router.post('/', auth, bookController.createBook);
 
 // GET de la liste des livres
-router.get("/", bookController.getBooks);
+router.get("/", auth, bookController.getBooks);
 
 // GET d'un livre spécifique
-router.get("/:id", bookController.getOneBook);
+router.get("/:id", auth, bookController.getOneBook);
 
 // PUT d'un livre modifié
-router.put("/:id", bookController.updateBook);
+router.put("/:id", auth, bookController.updateBook);
 
 //DELETE d'un livre
-router.delete("/:id", bookController.deleteBook);
+router.delete("/:id", auth, bookController.deleteBook);
 
 module.exports = router;
