2c56e06407dc1d5571a3a5168bab596e6bb719c0
AlistairFR <jbwinckel62370@gmail.com>
2023-07-12 11:37:37 +0200

    AJOUT routes et controllers books

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const Book = require("./models/Book");
+const bookRoutes = require("./routes/book");
 
 mongoose.connect('mongodb+srv://oc_projet7_admin:adminp7oc@cluster0.xnqr89c.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -21,30 +21,6 @@ app.use((req, res, next) => {
     next();
 });
 
-// POST d'un nouveau livre
-app.post('/api/books', (req, res, next) => {
-    const book = new Book({
-        ...req.body
-    });
-    book.save()
-        .then(() => res.status(201).json({ message : "Livre enregistré !" }))
-        .catch(error => res.status(400).json({ error }));
-    next();
-});
-
-// GET de la liste des livres
-app.get("/api/books", (req, res, next) => {
-    Book.find()
-        .then(books => res.status(200).json(books))
-        .catch(error => res.status(400).json({ error }));
-    next()
-})
-
-// GET d'un livre spécifique
-app.get("/api/books/:id", (req, res, next) => {
-    Book.findOne({ _id: req.params.id })
-        .then( book => res.status(200).json(book))
-        .catch(error => res.status(400).json({ error }));
-})
+app.use('/api/books', bookRoutes);
 
 module.exports = app;
---
+++ backend/routes/book.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const bookController = require("../controllers/book");
+
+// POST d'un nouveau livre
+router.post('/', bookController.createBook);
+
+// GET de la liste des livres
+router.get("/", bookController.getBooks);
+
+// GET d'un livre spécifique
+router.get("/:id", bookController.getBooks);
+
+// PUT d'un livre modifié
+router.put("/:id", bookController.updateBook);
+
+//DELETE d'un livre
+router.delete("/:id", bookController.deleteBook);
+
+module.exports = router;
