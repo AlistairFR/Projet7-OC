28807ec3d8cba3ddf14bfe0c9cc9470e6894abde
AlistairFR <jbwinckel62370@gmail.com>
2023-07-12 15:20:05 +0200

    FIX ajout livre et path imae

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -3,11 +3,12 @@ const fs = require("fs");
 
 exports.createBook = (req, res, next) => {
     const bookObject = JSON.parse(req.body.book);
+    delete bookObject._id;
     delete bookObject._userId;
     const book = new Book({
         ...bookObject,
         userId: req.auth.userId,
-        imageUrl: `${req.protocol}://${req.get('host')}/image/${req.file.filename}`
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     book.save()
         .then(() => {res.status(201).json({ message : "Livre enregistré !" })})
