cc1c37ff1972dbd308e6e2e3159945bee0a3951f
AlistairFR <jbwinckel62370@gmail.com>
2023-07-12 11:05:56 +0200

    AJOUT Routes livres POST, GET, GETone

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const Book = require("./models/Book");
+
 mongoose.connect('mongodb+srv://oc_projet7_admin:adminp7oc@cluster0.xnqr89c.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -11,6 +13,7 @@ const app = express();
 
 app.use(express.json());
 
+//Permet les requêtes cross-origin depuis n'importe quel domaine
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -18,10 +21,30 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/auth/signup', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({message : 'objet créé'});
+// POST d'un nouveau livre
+app.post('/api/books', (req, res, next) => {
+    const book = new Book({
+        ...req.body
+    });
+    book.save()
+        .then(() => res.status(201).json({ message : "Livre enregistré !" }))
+        .catch(error => res.status(400).json({ error }));
     next();
 });
+
+// GET de la liste des livres
+app.get("/api/books", (req, res, next) => {
+    Book.find()
+        .then(books => res.status(200).json(books))
+        .catch(error => res.status(400).json({ error }));
+    next()
+})
+
+// GET d'un livre spécifique
+app.get("/api/books/:id", (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+        .then( book => res.status(200).json(book))
+        .catch(error => res.status(400).json({ error }));
+})
 
 module.exports = app;
