53148135639f9c551b6012ea2141f1b4b0ddd8be
AlistairFR <jbwinckel62370@gmail.com>
2023-07-12 18:31:08 +0200

    FIX routes rateBook

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -9,7 +9,7 @@ const bookController = require("../controllers/book");
 router.get("/", bookController.getBooks);
 router.get("/bestrating", bookController.getBestBooks);
 router.get("/:id", bookController.getOneBook);
-router.get("/:id/rating", auth, bookController.rateBook);
+router.post("/:id/rating", auth, bookController.rateBook);
 router.post("/", auth, multer, bookController.createBook);
 router.put("/:id", auth, multer, bookController.updateBook);
 router.delete("/:id", auth, bookController.deleteBook);
