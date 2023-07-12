29fd859f17df4ad0f7b4f18397270f7ca6103444
AlistairFR <jbwinckel62370@gmail.com>
2023-07-12 10:22:59 +0200

    AJOUT Mongoose et connexion MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb+srv://oc_projet7_admin:adminp7oc@cluster0.xnqr89c.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
-    console.log("Reçue");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-})
-
-app.use((req, res, next) => {
-    res.json({ message : 'Votre requête a été reçue'});
+app.post('/api/auth/signup', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({message : 'objet créé'});
     next();
 });
-
-app.use((req, res) => {
-    console.log('Réponse envoyée avec succès');
-})
 
 module.exports = app;
