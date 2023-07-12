5843797b65446a906503e08916081a7383390c65
AlistairFR <jbwinckel62370@gmail.com>
2023-07-12 19:29:48 +0200

    FIX middleware sharp

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
-const sharp = require("sharp");
+const sharp = require("../middleware/sharp");
 
 const bookController = require("../controllers/book");
 
